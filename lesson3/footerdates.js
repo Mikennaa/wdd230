0b9c6b178400e6673118ce5f637993be4ab8b8e9
mikennaa <atk20006@byui.edu>
2021-09-30 22:10:25 -0600

    weather site content

---
+++ lesson3/footerdates.js
@@ -0,0 +1,4 @@
+const copyrightyear=document.querySelector("#copyrightyear");
+copyrightyear.textContent=new Date().getFullYear();
+const lastupdate=document.lastModified;
+document.getElementById("lastupdate").textContent=lastupdate;
