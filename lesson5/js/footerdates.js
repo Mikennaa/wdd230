23f030111b537561c0bb937ff3978c2ce480b736
mikennaa <atk20006@byui.edu>
2021-10-23 13:21:25 -0600

    formatting

--- lesson5/js/footerdates.js
+++ lesson5/js/footerdates.js
@@ -1,11 +1,3 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click',()=> {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize=()=> {if (window.innerWidth>760) mainnav.classList.remove('responsive')};
-
 let d = new Date();
 document.getElementById("copyrightyear").textContent = d.getFullYear();
 
@@ -18,22 +10,18 @@ const options = {
 
 document.getElementById("currentDate").textContent=d.toLocaleDateString('en-UK', options);
 
-const fri=d.getDay();
-const banner=document.querySelector("aside");
-if (fri==5) {
-  banner.setAttribute("class", "psaturday");
-} else {
-  banner.setAttribute("class", "notfriday");
+function bannerCalc() {
+
+  const d=newDate();
+  const today=d.getDay();
+  const pancake=document.querySelector(".pfriday");
+
+  if (today==5) {
+    pancake.style.display=block;
+  }
+else {
+  pancake.style.display='none';
 }
+}
+bannerCalc();
 
-
-
-
-
-/*** Programming Notes **************************************
-  Arrow Functions - es6 syntactically compact alternative to a regular function expression
-  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-  or https://www.w3schools.com/js/js_arrow_function.asp
-
-  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-  */
---
+++ lesson5/js/hamburger.js
@@ -0,0 +1,3 @@
+function toggleMenu() {
+    document.getElementById("mainnav").classList.toggle("hide");
+}
