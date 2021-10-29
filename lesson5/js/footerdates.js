bfaa5450fbec27f7686c3ac0f2bb2c004502c96d
mikennaa <atk20006@byui.edu>
2021-10-29 17:54:55 -0600

    contrast errors

--- lesson5/js/footerdates.js
+++ lesson5/js/footerdates.js
@@ -1,28 +1,28 @@
-let d = new Date();
-document.getElementById("copyrightyear").textContent = d.getFullYear();
-
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-}
-
-document.getElementById("currentDate").textContent=d.toLocaleDateString('en-UK', options);
-
-function bannerpopup() {
-
-  const d=newDate();
-  const today=d.getDay();
-  const pancake=document.querySelector(".pancakefriday");
-
-  if (today==5) {
-    pancake.style.display='block';
+  let d = new Date();
+  document.getElementById("copyrightyear").textContent = d.getFullYear();
+  
+  const options = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
   }
-
-  else {
-    pancake.style.display='none';
-  }
-  }
-  bannerpopup();
-
+  
+  document.getElementById("currentDate").textContent=d.toLocaleDateString('en-UK', options);
+  
+  function bannerpopup() {
+  
+    const d=new Date();
+    const today=d.getDay();
+    const pancake=document.querySelector(".pancakefriday");
+  
+    if (today==5) {
+      pancake.style.display='block';
+    }
+  
+    else {
+      pancake.style.display='none';
+    }
+    }
+    bannerpopup();
+  
