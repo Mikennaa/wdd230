2de80c04d4ddedf301a9ddda425ba42e485bcb4a
mikennaa <atk20006@byui.edu>
2021-10-30 01:55:47 -0600

    css updates

---
+++ lesson7/js/footerdates.js
@@ -0,0 +1,37 @@
+let d = new Date();
+document.getElementById("copyrightyear").textContent = d.getFullYear();
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+}
+
+document.getElementById("currentDate").textContent=d.toLocaleDateString('en-UK', options);
+
+  
+const pancake=document.querySelector(".pancakefriday");
+
+  if (d.getDay==5) {
+    pancake.style.display='block';
+  }
+
+  else {
+    pancake.style.display='none';
+  }
+  }
+  
+
+  const lastView = localStorage.getItem('lastView') || today; 
+  const displayDate = '';
+  const millisecondsToDays = 8640000;
+  if (lastView == today) {
+      document.querySelector('#visits').textContent = `Last view date: Congrats! It's your first time viiewing!`;
+  }
+  else {
+      displayDate = (lastVisit - today / millisecondsToDays).toFixed(0);
+      document.querySelector('#lastView').textContent =  `Last visit: ${displayDate}`;
+  }
+   localStorage.setItem('lastView', today); 
+      
